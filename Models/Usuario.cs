5bf099397bc992e4f14b9fed7e2917a9c65ef973
aracelyilaeni-colque <aracelyilaeni@gmail.com>
2026-04-10 15:19:02 -0300

    add projeto

---
+++ Models/Usuario.cs
@@ -0,0 +1,32 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Segundo_App_BancoDados.Models
+{
+    public class Usuario
+    {
+        [Display(Name ="Código")]
+        public int? idUsu { get; set; }
+
+        [Display(Name ="Nome Completo")]
+        [Required(ErrorMessage ="Campo nome é obrigatorio")]
+        public string NomeUsu { get; init; }
+
+        [Display(Name = "Cargo")]
+        [Required(ErrorMessage = "Campo Cargo é obrigatorio")]
+        public string Cargo { get; init; }
+
+        [Display(Name ="Data Nascimento")]
+        [Required(ErrorMessage = " Data Nascimento é obrigatorio")]
+        [DataType(DataType.DateTime)]
+        public DateTime DataNasc { get; init; }
+
+
+        
+
+    } 
+        
+
+}
+
