dacb18e14ddb779b37e6aacbc2a260259f9e471e
aracelyilaeni-colque <aracelyilaeni@gmail.com>
2026-04-17 15:34:32 -0300

    Update projeto add repository

---
+++ Repository/Contratc/IUsuarioRepository.cs
@@ -0,0 +1,22 @@
+﻿using Segundo_App_BancoDados.Models;
+
+namespace Segundo_App_BancoDados.Repository.Contratc
+{
+    public interface IUsuarioRepository
+    {
+        // CRUD - Create, Read, Update, Delete
+
+        IEnumerable<Usuario> ObterTodosUsuarios();
+
+        void Cadastrar(Usuario usuario);
+
+        void Atualizar(Usuario usuario);
+
+        Usuario ObterUsuario(int Id);
+
+        void Excluir(int id);
+
+
+  
+    }
+}
